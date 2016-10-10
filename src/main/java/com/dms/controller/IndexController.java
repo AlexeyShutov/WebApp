0acca5bbcb746a0d950d0adc85bda8625f235aab
Alexey <alexey.sh91@gail.com>
2016-10-11 00:06:04 +0500

    Index.html first draft & defaultSuccessUrl config

--- src/main/java/com/dms/controller/IndexController.java
+++ src/main/java/com/dms/controller/IndexController.java
@@ -1,13 +1,15 @@
 package com.dms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/index")
-    public String index() {
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index(ModelMap modelMap) {
         return "index";
     }
 }
